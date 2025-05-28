ae7bd9fe754de35cf7e06fb96cd21f935b6faf22
NutritionalYeet <audvic12@gmail.com>
2025-05-28 09:13:13 -0700

    First commit

---
+++ CSD 268 Final Part 1/CSD 268 Final Part 1/Driver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CSD_268_Final_Part_1
+{
+	public class Driver {
+
+		public bool CanDrive (int age)
+				{
+
+					const int drivingAge = 16; 
+
+					return age >= drivingAge;
+
+				}
+	}
+}
+
