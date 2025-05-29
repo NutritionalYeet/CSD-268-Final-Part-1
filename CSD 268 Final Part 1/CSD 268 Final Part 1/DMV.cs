f4eb4008931a605c64ee5cafb94adffa6619944b
NutritionalYeet <audvic12@gmail.com>
2025-05-29 13:24:11 -0700

    Typo fix

---
+++ CSD 268 Final Part 1/CSD 268 Final Part 1/DMV.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace CSD_268_Final_Part_1
+{
+	/// <summary>
+	/// Department of Motor Vehicles.
+	/// Contains the "CanDrive" method.
+	/// </summary>
+	public class DMV {
+
+		/// <summary>
+		/// Determines whether a person of a given age is able to drive.
+		/// Assumes the given age is that of a human.
+		/// </summary>
+		/// <param name="age">The potential driver's age</param>
+		/// <returns>Whether the provided age is at least 16.</returns>
+		public bool CanDrive (int age)
+				{
+
+					const int drivingAge = 16; 
+
+					return age >= drivingAge;
+
+				}
+	}
+}
+
--- CSD 268 Final Part 1/CSD 268 Final Part 1/UnitTestCanDrive.cs
+++ CSD 268 Final Part 1/CSD 268 Final Part 1/UnitTestCanDrive.cs
@@ -3,54 +3,54 @@
 public class DriverTests
 {
 
-	Driver driver;
+	DMV dmv;
 
 	[SetUp]
     public void Setup()
     {
-		driver = new Driver ();
+		dmv = new DMV ();
     }
 
 	[Test]
 	public void TestMinInt ()
 	{
-		Assert.AreEqual (driver.CanDrive (int.MinValue), false);
+		Assert.AreEqual (dmv.CanDrive (int.MinValue), false);
 	}
 
 	[Test]
 	public void TestMinIntPlusOne ()
 	{
-		Assert.AreEqual (driver.CanDrive (int.MinValue + 1), false);
+		Assert.AreEqual (dmv.CanDrive (int.MinValue + 1), false);
 	}
 
 	[Test]
     public void Test15()
     {
-        Assert.AreEqual(driver.CanDrive(15),false);
+        Assert.AreEqual(dmv.CanDrive(15),false);
     }
 
 	[Test]
 	public void Test16 ()
 	{
-		Assert.AreEqual (driver.CanDrive (16), true);
+		Assert.AreEqual (dmv.CanDrive (16), true);
 	}
 
 	[Test]
 	public void Test17 ()
 	{
-		Assert.AreEqual (driver.CanDrive (17), true);
+		Assert.AreEqual (dmv.CanDrive (17), true);
 	}
 
 	[Test]
 	public void TestMaxIntMinusOne ()
 	{
-		Assert.AreEqual (driver.CanDrive (int.MaxValue - 1), true);
+		Assert.AreEqual (dmv.CanDrive (int.MaxValue - 1), true);
 	}
 
 	[Test]
 	public void TestMaxInt ()
 	{
-		Assert.AreEqual (driver.CanDrive (int.MaxValue), true);
+		Assert.AreEqual (dmv.CanDrive (int.MaxValue), true);
 	}
 
 }
